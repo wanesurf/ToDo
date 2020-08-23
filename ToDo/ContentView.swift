772257a9fc585d715c9e7dadc6a880d8edca3a02
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2020-08-23 17:43:21 -0400

    Initial Commit

---
+++ ToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by Helwan Mandé on 2020-08-23.
+//  Copyright © 2020 Helwan Mandé. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

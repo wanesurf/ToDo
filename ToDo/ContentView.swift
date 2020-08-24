b670096966d46991f7539d296a989cc15cf18bad
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2020-08-23 21:03:00 -0400

    TasksDoe View Ok

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -9,16 +9,29 @@
 import SwiftUI
 var rowHeight: CGFloat = 50
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var managedContextObject
+    
+    @FetchRequest(entity: ToDoItems.entity(),
+                  sortDescriptors: [NSSortDescriptor(keyPath: \ToDoItems.createdAt, ascending: false)], predicate: NSPredicate(format: "taskDone = %d", false))
+    //important: pass the fetched data to another variable for holding the to-do items.
+    var fetchedItems: FetchedResults<ToDoItems>
+    
     @State var newTaskTitle = ""
-    var sampleTasks = ["Task One","Task Two","Task Three"]
+
+    //var sampleTasks = ["Task One","Task Two","Task Three"]
+    
     var body: some View {
-   
+        NavigationView{
             List{
-                ForEach(sampleTasks, id: \.self) { item in
+                ForEach(fetchedItems, id: \.self) { item in
                 HStack {
-                Text(item)
+                    Text(item.taskTitle ?? "Empty")
                 Spacer()
-                    Button(action: {print("Task Done")}){
+                    Button(action: {
+                        self.markTaskAsDone(at:
+                        self.fetchedItems.firstIndex(of: item)!)
+                        
+                    }){
                 Image(systemName: "circle")
                         .imageScale(.large)
                         .foregroundColor(.gray)
@@ -32,22 +45,61 @@ struct ContentView: View {
                 .frame(height: rowHeight)
                 //statics
                 HStack {
-                    TextField("Add task .. ", text: $newTaskTitle, onCommit: {print("New task added")})
+                    TextField("Add task .. ", text: $newTaskTitle, onCommit:{self.saveTask()})
+                    //save also by clicking the return button
                     
-                    Button(action: {print("New Task entered")}) {
+                Button(action: {self.saveTask()}) {
                         
                         Image(systemName: "plus")
                             .imageScale(.large)
                     }
                 }
                 .frame(height: rowHeight)
+                
+                HStack{
+                           Text("Completed Tasks")
+                       }
             }
+        .navigationBarTitle(Text("To-Do"))
+        .listStyle(GroupedListStyle())
+    }
+   
         }
+    func saveTask(){
+        //unwrap those input values with guard first.
+        guard self.newTaskTitle != "" else {
+            return
+        }
+        let newToDoItem = ToDoItems(context: self.managedContextObject)
+        newToDoItem.taskTitle = self.newTaskTitle
+        newToDoItem.createdAt = Date()
+        newToDoItem.taskDone = false
+        
+        do {
+            try self.managedContextObject.save()
+        }catch {print(error.localizedDescription)
+        }
+        self.newTaskTitle = ""
+    }
+    func markTaskAsDone(at index: Int){
+        let item = fetchedItems[index]
+        item.taskDone = true
+        
+        do {
+            try self.managedContextObject.save()
+        }catch{
+            print(error.localizedDescription)
+        }
+    }
     }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        return  ContentView().environment(\.managedObjectContext, context)
+       
     }
 }
